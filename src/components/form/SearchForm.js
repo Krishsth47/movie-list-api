d894a2b3249f662fbea7bbec8cd298404e448e53
Krishna Shrestha <krish_sth2005@yahoo.com>
2022-03-29 08:32:51 +1100

    search form working

--- src/components/form/SearchForm.js
+++ src/components/form/SearchForm.js
@@ -1,15 +1,36 @@
-import React from "react";
-import { Button, Col, Form, Row } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, Col, Form, Row, Alert } from "react-bootstrap";
 import { CustomCard } from "../card/CustomCard";
+import { fetchMovie } from "../../helper/axiosHelper";
 
 export const SearchForm = () => {
+  const [search, setSearch] = useState("");
+  const [movie, setMovie] = useState({});
+
+  const handleOnChange = (e) => {
+    const { value } = e.target;
+    setSearch(value);
+  };
+
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+
+    const movie = await fetchMovie(search);
+    console.log(movie);
+    setMovie(movie.data);
+  };
+
   return (
     <>
-      <Form>
+      <Form onSubmit={handleOnSubmit}>
         <Row>
           <Col></Col>
           <Col>
-            <Form.Control placeholder="Search ..." />
+            <Form.Control
+              placeholder="Search ..."
+              onChange={handleOnChange}
+              required
+            />
           </Col>
           <Col>
             <Button variant="warning" type="submit">
@@ -19,8 +40,11 @@ export const SearchForm = () => {
         </Row>
       </Form>
       <Row>
-        <Col className="d-flex justify-content-center">
-          <CustomCard />
+        <Col className="d-flex justify-content-center mt-3">
+          {movie.Response === "True" && <CustomCard movie={movie} />}
+          {movie.Response === "False" && (
+            <Alert variant="danger">{movie.Error}</Alert>
+          )}
         </Col>
       </Row>
     </>
